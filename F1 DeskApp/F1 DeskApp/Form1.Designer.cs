7bde2599d59945507f9ced31d0aafb3ce60701b7
Jonas Winston Hjorth Johansen <jowihjo@gmail.com>
2020-03-22 22:14:38 +0100

    Nå en (relativt) fungerende search-UI, tabell velges gjennom dropdown, som fyller dropdown nr.2 med søyler å velge, og til slutt settes søkeord i tekstboks og søk kan utføres

--- F1 DeskApp/F1 DeskApp/Form1.Designer.cs
+++ F1 DeskApp/F1 DeskApp/Form1.Designer.cs
@@ -33,14 +33,18 @@ namespace F1_DeskApp
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TableSelect = new System.Windows.Forms.ComboBox();
+            this.SearchTerm = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ColumnSelect = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(196, 66);
+            this.button1.Location = new System.Drawing.Point(8, 10);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 32);
@@ -62,12 +66,12 @@ namespace F1_DeskApp
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 208);
+            this.button2.Location = new System.Drawing.Point(188, 208);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 36);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Execute";
+            this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
@@ -90,33 +94,68 @@ namespace F1_DeskApp
             this.dataGridView1.Size = new System.Drawing.Size(719, 424);
             this.dataGridView1.TabIndex = 6;
             // 
+            // TableSelect
+            // 
+            this.TableSelect.FormattingEnabled = true;
+            this.TableSelect.Location = new System.Drawing.Point(193, 90);
+            this.TableSelect.Name = "TableSelect";
+            this.TableSelect.Size = new System.Drawing.Size(121, 21);
+            this.TableSelect.TabIndex = 8;
+            this.TableSelect.Text = "Select Table";
+            this.TableSelect.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // SearchTerm
+            // 
+            this.SearchTerm.Location = new System.Drawing.Point(193, 171);
+            this.SearchTerm.Name = "SearchTerm";
+            this.SearchTerm.Size = new System.Drawing.Size(126, 20);
+            this.SearchTerm.TabIndex = 9;
+            this.SearchTerm.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(8, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(8, 217);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(190, 137);
+            this.label1.Location = new System.Drawing.Point(193, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Write your SQL query here";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Search Term";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // comboBox1
+            // ColumnSelect
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 126);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 8;
-            this.comboBox1.Text = "Select Table";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.ColumnSelect.FormattingEnabled = true;
+            this.ColumnSelect.Location = new System.Drawing.Point(193, 117);
+            this.ColumnSelect.Name = "ColumnSelect";
+            this.ColumnSelect.Size = new System.Drawing.Size(121, 21);
+            this.ColumnSelect.TabIndex = 13;
+            this.ColumnSelect.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1148, 448);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ColumnSelect);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.SearchTerm);
+            this.Controls.Add(this.TableSelect);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -137,8 +176,12 @@ namespace F1_DeskApp
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox TableSelect;
+        private System.Windows.Forms.TextBox SearchTerm;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox ColumnSelect;
     }
 }
 
